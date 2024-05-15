ea0fd8e301774b33178b13832f61e1120b45d676
Leonardo Paschoa <78983450+LeonardoPaschoaBraga16@users.noreply.github.com>
2024-05-15 02:30:43 -0300

    15/05/2024 02:29 ESQUELETO CRIADO
    Criado o esqueleto base da aplicaçao Django. Nada funcional, apenas a estrutura básica. Aguardando feedback para continuar com o desenvolvimento e adição de funcionalidades

--- login/urls.py
+++ login/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("login/", index)
+    path("login/", index),
+    path("pedidos/", pedidos),
 ]
--- login/views.py
+++ login/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def pedidos(request):
+    return render(request, 'orderTable.html')
