810535305809429d4f781b63a9dd259015d51b48
Leonardo Paschoa <78983450+LeonardoPaschoaBraga16@users.noreply.github.com>
2024-05-15 03:41:38 -0300

    15/05/2024 03:40 SISTEMA DE LOGIN IMPLEMENTADO
    Sistema de Login e autenticação implementado utilizando do Django Authenticate. Sendo assim, agora é possivel realizar a autenticação de usuarios, por meio dos usuarios cadastrados no Django Admin. Para que assim se possa ter mais controle de quem pode ou não acessar o sistema

--- login/views.py
+++ login/views.py
@@ -1,7 +1,22 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
 
 def index(request):
-    return render(request, 'index.html')
+    if request.method == "GET":
+        return render(request, 'index.html')
+    else:
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = authenticate(username=username, password=senha)
+
+        if user:
+            return render(request, 'orderTable.html')
+        else:
+            return HttpResponse('Nome ou Senha errados')
+
 
 def pedidos(request):
     return render(request, 'orderTable.html')
