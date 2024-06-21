078ecdbf8cdcb927d4d257f8de5c435c744efd88
Leonardo Paschoa <78983450+LeonardoPaschoaBraga16@users.noreply.github.com>
2024-06-21 02:34:06 -0300

    21/06/2024 02:32 IMPLEMENTAÇÃO API
    IMPLEMENTAÇÃO API AVANÇOU, ADICIONEI HORÁRIO E ARRUMEI ALGUNS ERROS. ALÉM DE FILTRAR COMO FUNCIONARÁ A SELEÇÃO DOS PEDIDOS

--- login/handlers/orders.py
+++ login/handlers/orders.py
@@ -1,5 +1,6 @@
 import requests
 from django.shortcuts import render
+from datetime import datetime, timedelta
 
 API_BASE_URL = 'https://lhcapi.azurewebsites.net/api'
 
@@ -19,26 +20,44 @@ def get_all_orders():
         print("Falha ao recuperar pedidos.")
         return None
 
+from datetime import datetime
+
 def order_list_view(request):
     orders = get_all_orders()
 
     if orders:
         order_details = []
+        current_datetime = datetime.now()
+
         for order in orders:
+            # Converter createdAt para datetime
+            created_at = order.get('createdAt')
+            created_at_datetime = datetime.fromisoformat(created_at)
+
+            # Calcular a diferença de tempo
+            time_difference = current_datetime - created_at_datetime
+
+            # Formatar a diferença para horas e minutos
+            hours = time_difference // timedelta(hours=1)
+            minutes = (time_difference // timedelta(minutes=1)) % 60
+
+            # Montar o dicionário com os detalhes do pedido
             order_info = {
                 'id': order.get('id'),
                 'idClient': order.get('idClient'),
                 'totalPrice': order.get('totalPrice'),
                 'status': order.get('status'),
                 'tableNumber': order.get('tableNumber'),
-                'createdAt': order.get('createdAt'),
-                'updatedAt': order.get('updatedAt')
+                'createdAt': created_at_datetime,  # Manter o objeto datetime para outros usos
+                'updatedAt': order.get('updatedAt'),
+                'timeDifference': f"{hours:02}h{minutes:02}min"  # Formatar horas e minutos
             }
             order_details.append(order_info)
 
         return order_details
     else:
         return None
+
 
 
 def get_order_by_id(order_id):
